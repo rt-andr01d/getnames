4d71716cd13ff8adfad946e5e2c6d86a6af48002
andr01d <nottellingyou@email.com>
2021-01-10 21:18:56 -0600

    Requests use libraries now instead of relying on bash commands

--- getnames.py
+++ getnames.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import bs4
+import requests
 import argparse
 import sys
 import re
@@ -7,7 +9,7 @@ import re
 ###python getname.py -f file.txt -e <fn>.<ln>@email.com
 
 parser = argparse.ArgumentParser(description='Create emails based on a google search result')
-parser.add_argument('-f', '--file', help='Text file containing google search', required=True)
+parser.add_argument('-c', '--company', help='Name of target organization', required=True)
 parser.add_argument('-e', '--emailformat', help='Format you would like the email in. e.g. <fn>.<ln>@gmail.com', required=True)
 parser.add_argument('-o', '--outfile', help='File to write output to')
 args = parser.parse_args()
@@ -15,7 +17,7 @@ args = parser.parse_args()
 if args.outfile:
 	outfile = open(sys.argv[6], "w")
 
-file = open(sys.argv[2], "r")
+#file = open(sys.argv[2], "r")
 
 fname = ""
 lname = ""
@@ -38,18 +40,29 @@ def returnformat(fname, lname):
 			email = lname + fname + format[4]
 			return email
 
-for line in file:
-	splitline = line.split(" ")
-	if len(splitline) > 2:
-		if splitline[2] == "-":
-			fname = splitline[0]
-			lname = splitline[1]
-			email = returnformat(fname, lname)
-			if args.outfile:
-				outfile.write(email)
-				outfile.write("\n")
-			else:
-				print(email)
-			
+#for line in file:
+#	splitline = line.split(" ")
+#	if len(splitline) > 2:
+#		if splitline[2] == "-":
+#			fname = splitline[0]
+#			lname = splitline[1]
+#			email = returnformat(fname, lname)
+#			if args.outfile:
+#				outfile.write(email)
+#				outfile.write("\n")
+#			else:
+#				print(email)
 
+
+url = 'https://google.com/search?q=site:linkedin.com%20"' + sys.argv[2] + '"&num=100'
+request_result=requests.get( url ) 
+soup = bs4.BeautifulSoup(request_result.text, "html.parser") 
+output = re.findall(r'[a-zA-Z]{1,7} [a-zA-Z]{1,7} -', soup.prettify())
+
+for line in output:
+	splitline = line.split(" ")
+	fname = splitline[0]
+	lname = splitline[1]
+	email = returnformat(fname, lname)
+	print(email)
       
--- test.py
+++ test.py
@@ -7,11 +7,13 @@ url = 'https://google.com/search?q=site:linkedin.com%20"RSM US LLP"&num=100'
     
 request_result=requests.get( url ) 
      
-soup = bs4.BeautifulSoup(request_result.text, 
-                                       "html.parser") 
+soup = bs4.BeautifulSoup(request_result.text, "html.parser") 
 
-print(soup, file=open("out.txt", "a"))
+output = re.findall(r'[a-zA-Z]{1,7} [a-zA-Z]{1,7} -', soup.prettify())
 
-os.system("egrep -o -E '[[:alnum:]]{3,7} [[:alnum:]]{3,7} -' out.txt > names.txt")
+for line in output:
+    print(line)
 
-os.remove("out.txt")
+#os.system("egrep -o -E '[[:alnum:]]{3,7} [[:alnum:]]{3,7} -' out.txt > names.txt")
+
+#os.remove("out.txt")
